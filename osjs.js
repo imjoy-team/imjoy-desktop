4ed76352e3f928647b5674fd7c3fbdd4ddbe19e4
oeway <oeway007@gmail.com>
2021-12-10 20:18:46 +0000

    Deploying to gh-pages from @ imjoy-team/imjoy-desktop@9918cb4fd3c1a47e4eda1b912f4ba5dee48e2542 🚀

--- osjs.js
+++ osjs.js
@@ -299,9 +299,11 @@ function removeMountpoint(core, name) {
   }
 }
 
-function registerApp(pm, _ref) {
+function registerApp(osjs, _ref) {
   var name = _ref.name,
       category = _ref.category,
+      _ref$icon = _ref.icon,
+      icon = _ref$icon === void 0 ? null : _ref$icon,
       _ref$singleton = _ref.singleton,
       singleton = _ref$singleton === void 0 ? false : _ref$singleton,
       run = _ref.run;
@@ -311,7 +313,7 @@ function registerApp(pm, _ref) {
     singleton: singleton,
     autostart: false,
     hidden: false,
-    icon: null,
+    icon: icon,
     category: category,
     groups: [],
     title: {
@@ -321,10 +323,33 @@ function registerApp(pm, _ref) {
     description: {
       "en_EN": name
     }
-  }; // const pkgs = new Packages(osjs);
+  };
+  var pm = osjs.make('osjs/packages'); // const pkgs = new Packages(osjs);
 
   pm.addPackages([metadata]);
-  pm.register(name, run);
+  pm.register(name, /*#__PURE__*/_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee() {
+    var proc;
+    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            proc = new _osjs_client__WEBPACK_IMPORTED_MODULE_3__["Application"](osjs, {
+              args: {},
+              metadata: metadata
+            });
+            _context.next = 3;
+            return run(metadata);
+
+          case 3:
+            return _context.abrupt("return", proc);
+
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  })));
 }
 
 var init = function init() {
@@ -346,24 +371,23 @@ var init = function init() {
   osjs.register(_osjs_dialogs__WEBPACK_IMPORTED_MODULE_7__["DialogServiceProvider"]);
   osjs.register(_osjs_gui__WEBPACK_IMPORTED_MODULE_6__["GUIServiceProvider"]);
   osjs.boot().then(function () {
-    var pm = osjs.make('osjs/packages');
     var imjoy = new imjoy_core__WEBPACK_IMPORTED_MODULE_2__["ImJoy"]({
       imjoy_api: {} //imjoy config
 
     });
     imjoy.start({
       workspace: 'default'
-    }).then( /*#__PURE__*/_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee9() {
-      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee9$(_context9) {
+    }).then( /*#__PURE__*/_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee10() {
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee10$(_context10) {
         while (1) {
-          switch (_context9.prev = _context9.next) {
+          switch (_context10.prev = _context10.next) {
             case 0:
               imjoy.event_bus.on("add_window", /*#__PURE__*/function () {
-                var _ref3 = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee(w) {
+                var _ref4 = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee2(w) {
                   var win;
-                  return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee$(_context) {
+                  return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee2$(_context2) {
                     while (1) {
-                      switch (_context.prev = _context.next) {
+                      switch (_context2.prev = _context2.next) {
                         case 0:
                           win = osjs.make('osjs/window', {
                             id: w.window_id,
@@ -384,70 +408,45 @@ var init = function init() {
 
                         case 2:
                         case "end":
-                          return _context.stop();
+                          return _context2.stop();
                       }
                     }
-                  }, _callee);
+                  }, _callee2);
                 }));
 
                 return function (_x) {
-                  return _ref3.apply(this, arguments);
+                  return _ref4.apply(this, arguments);
                 };
               }());
               console.log('ImJoy started');
-              registerApp(pm, {
+              registerApp(osjs, {
                 name: "Kaibu",
                 category: "science",
                 run: function run() {
-                  return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee2() {
-                    var viewer;
-                    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee2$(_context2) {
-                      while (1) {
-                        switch (_context2.prev = _context2.next) {
-                          case 0:
-                            _context2.next = 2;
-                            return imjoy.api.createWindow({
-                              src: "https://kaibu.org/#/app",
-                              name: "Kaibu"
-                            });
-
-                          case 2:
-                            viewer = _context2.sent;
-                            _context2.next = 5;
-                            return viewer.view_image("https://images.proteinatlas.org/61448/1319_C10_2_blue_red_green.jpg");
-
-                          case 5:
-                            _context2.next = 7;
-                            return viewer.add_shapes([], {
-                              name: "Annotation"
-                            });
-
-                          case 7:
-                          case "end":
-                            return _context2.stop();
-                        }
-                      }
-                    }, _callee2);
-                  }))();
-                }
-              });
-              registerApp(pm, {
-                name: "JupyterLite",
-                category: "science",
-                run: function run() {
                   return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee3() {
+                    var viewer;
                     return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee3$(_context3) {
                       while (1) {
                         switch (_context3.prev = _context3.next) {
                           case 0:
                             _context3.next = 2;
                             return imjoy.api.createWindow({
-                              src: "https://jupyter.imjoy.io",
-                              name: "JupyterLite",
-                              passive: true
+                              src: "https://kaibu.org/#/app",
+                              name: "Kaibu"
                             });
 
                           case 2:
+                            viewer = _context3.sent;
+                            _context3.next = 5;
+                            return viewer.view_image("https://images.proteinatlas.org/61448/1319_C10_2_blue_red_green.jpg");
+
+                          case 5:
+                            _context3.next = 7;
+                            return viewer.add_shapes([], {
+                              name: "Annotation"
+                            });
+
+                          case 7:
                           case "end":
                             return _context3.stop();
                         }
@@ -456,8 +455,8 @@ var init = function init() {
                   }))();
                 }
               });
-              registerApp(pm, {
-                name: "Code",
+              registerApp(osjs, {
+                name: "JupyterLite",
                 category: "science",
                 run: function run() {
                   return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee4() {
@@ -467,8 +466,8 @@ var init = function init() {
                           case 0:
                             _context4.next = 2;
                             return imjoy.api.createWindow({
-                              src: "https://vscode.dev",
-                              name: "Code",
+                              src: "https://jupyter.imjoy.io",
+                              name: "JupyterLite",
                               passive: true
                             });
 
@@ -481,8 +480,8 @@ var init = function init() {
                   }))();
                 }
               });
-              registerApp(pm, {
-                name: "elFinder",
+              registerApp(osjs, {
+                name: "Code",
                 category: "science",
                 run: function run() {
                   return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee5() {
@@ -492,8 +491,9 @@ var init = function init() {
                           case 0:
                             _context5.next = 2;
                             return imjoy.api.createWindow({
-                              src: "https://fm.imjoy.io",
-                              name: "elFinder"
+                              src: "https://vscode.dev",
+                              name: "Code",
+                              passive: true
                             });
 
                           case 2:
@@ -505,8 +505,8 @@ var init = function init() {
                   }))();
                 }
               });
-              registerApp(pm, {
-                name: "ImJoy Slides",
+              registerApp(osjs, {
+                name: "elFinder",
                 category: "science",
                 run: function run() {
                   return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee6() {
@@ -516,8 +516,8 @@ var init = function init() {
                           case 0:
                             _context6.next = 2;
                             return imjoy.api.createWindow({
-                              src: "https://slides.imjoy.io",
-                              name: "ImJoy Slides"
+                              src: "https://fm.imjoy.io",
+                              name: "elFinder"
                             });
 
                           case 2:
@@ -529,8 +529,8 @@ var init = function init() {
                   }))();
                 }
               });
-              registerApp(pm, {
-                name: "ImJoy Chart",
+              registerApp(osjs, {
+                name: "ImJoy Slides",
                 category: "science",
                 run: function run() {
                   return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee7() {
@@ -540,8 +540,8 @@ var init = function init() {
                           case 0:
                             _context7.next = 2;
                             return imjoy.api.createWindow({
-                              src: "https://chart.imjoy.io",
-                              name: "ImJoy Chart"
+                              src: "https://slides.imjoy.io",
+                              name: "ImJoy Slides"
                             });
 
                           case 2:
@@ -553,29 +553,22 @@ var init = function init() {
                   }))();
                 }
               });
-              registerApp(pm, {
-                name: "HPA UMAP Studio",
+              registerApp(osjs, {
+                name: "ImJoy Chart",
                 category: "science",
                 run: function run() {
                   return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee8() {
-                    var editor;
                     return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee8$(_context8) {
                       while (1) {
                         switch (_context8.prev = _context8.next) {
                           case 0:
                             _context8.next = 2;
                             return imjoy.api.createWindow({
-                              src: 'https://chart.imjoy.io',
-                              name: 'HPA UMAP Studio',
-                              fullscreen: true
+                              src: "https://chart.imjoy.io",
+                              name: "ImJoy Chart"
                             });
 
                           case 2:
-                            editor = _context8.sent;
-                            _context8.next = 5;
-                            return editor.loadDataSource("https://dl.dropbox.com/s/k9ekd4ff3fyjbfk/umap_results_fit_all_transform_all_sorted_20190422.csv");
-
-                          case 5:
                           case "end":
                             return _context8.stop();
                         }
@@ -584,13 +577,44 @@ var init = function init() {
                   }))();
                 }
               });
+              registerApp(osjs, {
+                name: "HPA UMAP Studio",
+                category: "science",
+                run: function run() {
+                  return _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee9() {
+                    var editor;
+                    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee9$(_context9) {
+                      while (1) {
+                        switch (_context9.prev = _context9.next) {
+                          case 0:
+                            _context9.next = 2;
+                            return imjoy.api.createWindow({
+                              src: 'https://chart.imjoy.io',
+                              name: 'HPA UMAP Studio',
+                              fullscreen: true
+                            });
+
+                          case 2:
+                            editor = _context9.sent;
+                            _context9.next = 5;
+                            return editor.loadDataSource("https://dl.dropbox.com/s/k9ekd4ff3fyjbfk/umap_results_fit_all_transform_all_sorted_20190422.csv");
+
+                          case 5:
+                          case "end":
+                            return _context9.stop();
+                        }
+                      }
+                    }, _callee9);
+                  }))();
+                }
+              });
 
             case 9:
             case "end":
-              return _context9.stop();
+              return _context10.stop();
           }
         }
-      }, _callee9);
+      }, _callee10);
     })));
   });
 };
